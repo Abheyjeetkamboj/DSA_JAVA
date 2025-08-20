62b040c6d31e40268295fdcc89c175168f4dd8a7
abheyjeet <abheyjeet100@gmail.com>
2025-08-20 18:18:54 +0530

    sorting array

--- Array/A6_sort.java
+++ Array/A6_sort.java
@@ -3,6 +3,7 @@ package Array;
 public class A6_sort {
     public static void main(String[] args) {
         int[] arr = {5,4,7,8,2};
+        // int[] arr1={2,45,1,3,7,5};
         for(int i = 0;i<arr.length;i++){
             for(int j = i+1;j<arr.length;j++){
                 if(arr[i]>arr[j]){
@@ -14,6 +15,7 @@ public class A6_sort {
         }
         for(int i = 0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
+
         }
     }
 }
