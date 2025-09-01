c85bb9919b9848bd336f9acb6b42b264bcd5c48d
abheyjeet <abheyjeet100@gmail.com>
2025-09-02 02:53:52 +0530

    lower case upper case

---
+++ Strings/A4_To_lower_case.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class A4_To_lower_case {
+    public static void main(String[] args) {
+        String name = "ABheyjeet KAMBOj";
+        // String lower = name.toLowerCase();
+        // System.out.println(lower);
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+
+
+        String a = "ABC";
+        String b = "dcs";
+        System.out.println(a.concat(b));//combines two strings;
+    }   
+}
