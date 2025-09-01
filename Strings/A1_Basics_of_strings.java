ca3c4cafd92d90235d3261853119970a9f5ba690
abheyjeet <abheyjeet100@gmail.com>
2025-09-02 02:51:53 +0530

    basics of string

---
+++ Strings/A1_Basics_of_strings.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class A1_Basics_of_strings {
+    public static void main(String[] args) {
+        String name = "Abheyjeet Kamboj";
+        System.out.println(name);
+        
+    }
+}
