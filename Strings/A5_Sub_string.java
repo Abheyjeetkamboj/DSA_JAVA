06e0626e53514fd7268e1b1215eb0379af6f7af8
abheyjeet <abheyjeet100@gmail.com>
2025-09-02 02:54:33 +0530

    Sub String

---
+++ Strings/A5_Sub_string.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class A5_Sub_string {
+    public static void main(String[] args) {
+        String s = "Abheyjeet";
+        System.out.println(s.substring(2));
+        System.out.println(s.substring(2, 6));
+    }
+}
