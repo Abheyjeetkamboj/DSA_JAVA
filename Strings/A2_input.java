6f5099944f87e87e6223dfec08e818afa5fa77bf
abheyjeet <abheyjeet100@gmail.com>
2025-09-02 02:52:20 +0530

    input

---
+++ Strings/A2_input.java
@@ -0,0 +1,12 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class A2_input {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        String name = sr.next();// for input 
+        System.out.println(name);//but this will take input only for first letter
+
+    }   
+}
