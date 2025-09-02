8eea2f776a68b86c2c1a218f39802fb041d2da02
abheyjeet <abheyjeet100@gmail.com>
2025-09-02 16:35:02 +0530

    plus operator

---
+++ Strings/A7_Plus_operator.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class A7_Plus_operator {
+    public static void main(String[] args) {
+        String s = "Abc";
+        s = s+"def";
+        s = s+'c';
+        s = s+10;
+        System.out.println(s);
+        System.out.println("Abheyjeet"+(10+20));
+        System.out.println("Abheyjeet"+10+20);
+    }
+}
+
