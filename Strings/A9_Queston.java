4652b643b31fb1bf8799e1bbc680ad5cc45973bf
abheyjeet <abheyjeet100@gmail.com>
2025-09-02 16:36:03 +0530

    Problem statement

---
+++ Strings/A9_Queston.java
@@ -0,0 +1,12 @@
+package Strings;
+import java.util.Scanner;
+public class A9_Queston {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        int x= sr.nextInt();
+        String y = Integer.toString(x);
+        System.out.println(y.length());
+        
+
+    }
+}
