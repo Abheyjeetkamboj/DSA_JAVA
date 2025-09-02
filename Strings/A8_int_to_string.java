f80dfbea1352254eccfaf8f75d63fff28b5419ba
abheyjeet <abheyjeet100@gmail.com>
2025-09-02 16:35:34 +0530

    Integer To String

---
+++ Strings/A8_int_to_string.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class A8_int_to_string {
+    public static void main(String[] args) {
+            Scanner sr = new Scanner(System.in);
+            int s = sr.nextInt();
+            // System.out.println(s+"");
+            String hello = s+"";
+            System.out.println(hello);
+    }
+}
