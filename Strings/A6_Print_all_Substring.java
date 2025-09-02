1bd34f7ed6f659e5d83bde84c6073b80e40c499f
abheyjeet <abheyjeet100@gmail.com>
2025-09-02 16:34:31 +0530

    Substrings

---
+++ Strings/A6_Print_all_Substring.java
@@ -0,0 +1,17 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class A6_Print_all_Substring {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        String s= sr.next();
+        for(int i = 0;i<s.length();i++){
+            for(int j = i+1;j<=s.length();j++){
+                System.out.print(s.substring(i,j)+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
